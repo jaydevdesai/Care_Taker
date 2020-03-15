4556a12324ba92112b246c25d8f742758a740e1e
Shivang Thaker <thake116@uwindsor.ca>
2020-03-15 22:31:18 +0000

    Update Admin/Service_View_Delete.php, Admin/CareTaker_Manage.php, Admin/CareTaker_EVD.php, Admin/Utility_Service_Delete.php, Admin/Utility_CT.php files

---
+++ Admin/CareTaker_Manage.php
@@ -0,0 +1,44 @@
+<!DOCTYPE HTML>
+<html>
+<?php
+require_once("Navigation.php");
+?>
+<head>
+    <title>Caretaker Manage</title>
+</head>
+<body>
+<div id="wrapper">
+    <div id="page-wrapper" style="margin-top: 98px;">
+        <div class="graphs">
+            <div class="xs">
+                <h3>Manage Caretaker</h3>
+                <div class="tab-content" style="margin-top: 80px;margin-bottom: 80px;">
+                    <div class="tab-pane active" id="horizontal-form">
+                        <div class="col-md-6 col-sm-6 col-xs-12 span_1_of_2">
+                            <a class="tiles_info" href="CareTaker_Add.php">
+                                <div class="tiles-body fb2" style="background-color: #3e6267; padding-top: 75px;padding-bottom: 75px;margin-top: 20px;margin-bottom: 20px;">
+                                    <div class="text-center" style="font-size: 24px;">ADD CARETAKER</div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-md-6 col-sm-12 col-xs-12 span_1_of_2">
+                            <a class="tiles_info" href="CareTaker_EVD.php">
+                                <div class="tiles-body fb2" style="background-color: #3e6267; padding-top: 75px;padding-bottom: 75px;margin-top: 20px;margin-bottom: 20px;">
+                                    <div class="text-center" style="font-size: 24px;">VIEW / DELETE CARETAKER</div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                </div>
+
+                <div class="copy_layout">
+
+                </div>
+            </div>
+        </div>
+        <!-- /#page-wrapper -->
+    </div>
+    <!-- /#wrapper -->
+</body>
+</html>
